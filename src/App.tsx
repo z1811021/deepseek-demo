f0899e82b3e3b663a529d9ce290f9da81fb1de3b
z1811021 <791487336@qq.com>
2025-06-04 11:22:52 +0800

    change to qwen

--- src/App.tsx
+++ src/App.tsx
@@ -9,113 +9,113 @@ import markdownit from "markdown-it";
 const md = markdownit({ html: true, breaks: true });
 
 const client = new OpenAI({
-  baseURL: "https://api.deepseek.com/v1",
-  // 模型名称
+	baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
+	// 模型名称
 
-  // apiKey: import.meta.env.VITE_DEEPSEEK_API_KEY,
-  apiKey: "sk-71b286b74c4f4285863d4e9f2861e8cb",
-  dangerouslyAllowBrowser: true,
+	// apiKey: import.meta.env.VITE_DEEPSEEK_API_KEY,
+	apiKey: "sk-df7515632a19474e80715111c8440c5e",
+	dangerouslyAllowBrowser: true,
 });
 const roles: GetProp<typeof Bubble.List, "roles"> = {
-  ai: {
-    placement: "start",
-    avatar: { icon: <UserOutlined />, style: { background: "#fde3cf" } },
-  },
-  local: {
-    placement: "end",
-    avatar: { icon: <UserOutlined />, style: { background: "#87d068" } },
-  },
+	ai: {
+		placement: "start",
+		avatar: { icon: <UserOutlined />, style: { background: "#fde3cf" } },
+	},
+	local: {
+		placement: "end",
+		avatar: { icon: <UserOutlined />, style: { background: "#87d068" } },
+	},
 };
 const renderMarkdown: BubbleProps["messageRender"] = (content) => {
-  return content === "" ? (
-    <Space>
-      <Spin size="small" />
-      Loading...
-    </Space>
-  ) : (
-    <Typography>
-      {/* biome-ignore lint/security/noDangerouslySetInnerHtml: used in demo */}
-      <div dangerouslySetInnerHTML={{ __html: md.render(content) }} />
-    </Typography>
-  );
+	return content === "" ? (
+		<Space>
+			<Spin size="small" />
+			Loading...
+		</Space>
+	) : (
+		<Typography>
+			{/* biome-ignore lint/security/noDangerouslySetInnerHtml: used in demo */}
+			<div dangerouslySetInnerHTML={{ __html: md.render(content) }} />
+		</Typography>
+	);
 };
 const App: React.FC = () => {
-  const [content, setContent] = React.useState("");
+	const [content, setContent] = React.useState("");
 
-  const [agent] = useXAgent({
-    request: async (info, callbacks) => {
-      const { messages, message } = info;
+	const [agent] = useXAgent({
+		request: async (info, callbacks) => {
+			const { messages, message } = info;
 
-      const { onSuccess, onUpdate } = callbacks;
+			const { onSuccess, onUpdate } = callbacks;
 
-      // current message
-      console.log("message", message);
+			// current message
+			console.log("message", message);
 
-      // history messages
-      console.log("messages", messages);
+			// history messages
+			console.log("messages", messages);
 
-      let content: string = "";
+			let content: string = "";
 
-      try {
-        const stream = await client.chat.completions.create({
-          model: "deepseek-reasoner",
-          // if chat context is needed, modify the array
-          messages: [{ role: "user", content: message ?? "" }],
-          // stream mode
-          stream: true,
-        });
+			try {
+				const stream = await client.chat.completions.create({
+					model: "qwen-plus",
+					// if chat context is needed, modify the array
+					messages: [{ role: "user", content: message ?? "" }],
+					// stream mode
+					stream: true,
+				});
 
-        for await (const chunk of stream) {
-          content += chunk.choices[0]?.delta?.content || "";
+				for await (const chunk of stream) {
+					content += chunk.choices[0]?.delta?.content || "";
 
-          onUpdate(content);
-        }
+					onUpdate(content);
+				}
 
-        onSuccess(content);
-      } catch (error) {
-        console.log("🚀 ~ request: ~ error:", error);
-        // handle error
-        // onError();
-      }
-    },
-  });
+				onSuccess(content);
+			} catch (error) {
+				console.log("🚀 ~ request: ~ error:", error);
+				// handle error
+				// onError();
+			}
+		},
+	});
 
-  const {
-    // use to send message
-    onRequest,
-    // use to render messages
-    messages,
-  } = useXChat({ agent });
+	const {
+		// use to send message
+		onRequest,
+		// use to render messages
+		messages,
+	} = useXChat({ agent });
 
-  return (
-    <Flex vertical gap="middle">
-      <Bubble.List
-        roles={roles}
-        items={messages.map(({ id, message, status }) => ({
-          key: id,
-          role: status === "local" ? "local" : "ai",
-          content: message,
-          messageRender: renderMarkdown,
-        }))}
-        autoScroll={true}
-        style={{
-          overflowY: "auto",
-          maxHeight: "80vh",
-          scrollbarWidth: "none", // Firefox
-          msOverflowStyle: "none", // IE 10+
-        }}
-      />
-      <Sender
-        loading={agent.isRequesting()}
-        value={content}
-        onChange={setContent}
-        onSubmit={(nextContent) => {
-          onRequest(nextContent);
-          setContent("");
-        }}
-      />
-    </Flex>
-  );
+	return (
+		<Flex vertical gap="middle">
+			<Bubble.List
+				roles={roles}
+				items={messages.map(({ id, message, status }) => ({
+					key: id,
+					role: status === "local" ? "local" : "ai",
+					content: message,
+					messageRender: renderMarkdown,
+				}))}
+				autoScroll={true}
+				style={{
+					overflowY: "auto",
+					maxHeight: "80vh",
+					scrollbarWidth: "none", // Firefox
+					msOverflowStyle: "none", // IE 10+
+				}}
+			/>
+			<Sender
+				loading={agent.isRequesting()}
+				value={content}
+				onChange={setContent}
+				onSubmit={(nextContent) => {
+					onRequest(nextContent);
+					setContent("");
+				}}
+			/>
+		</Flex>
+	);
 };
 
 export default App;
